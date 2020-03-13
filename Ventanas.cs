57fa26312b53d2e66e8340c7973bdfd48442452a
rpabogota <oscar.torres@grupokonecta.com>
2020-03-13 18:09:04 -0500

    Update Ventanas.cs

--- Ventanas.cs
+++ Ventanas.cs
@@ -1,7 +1,22 @@
-// Maximizar Ventana
+// Maximizar Ventana //
 
 driver.Manage().Window.Maximize();
 
-// Cambiar Posicion del navegador
+// Cambiar Posicion del navegador //
 
 driver.Manage().Window.Position = new Point(0, 0);
+
+// Cambiar pestaña por Index
+// la variable "index" corresponde a la posicion de la pestaña a la cual se cambiara el foco //
+
+ArrayList tabs = new ArrayList(driver.WindowHandles);
+driver.SwitchTo().Window(tabs[index].ToString());
+
+// Cambiar a la ultima pestaña abierta //
+
+ArrayList tabs = new ArrayList(driver.WindowHandles);
+driver.SwitchTo().Window(tabs[tabs.Count - 1].ToString());
+
+// Cerrar pop up con boton de aceptar //
+
+driver.SwitchTo().Alert().Accept();
